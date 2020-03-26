d8a9b73e2ab2baaf6700e8b2c3cbac802a521f39
tiffinschool <62676209+tiffinschool@users.noreply.github.com>
2020-03-26 08:23:00 +0000

    Updated from Brython Server: 26/03/2020 08:22:58

--- make_a_space_backdrop.py
+++ make_a_space_backdrop.py
@@ -12,7 +12,7 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 
-def makeBackground(color):
+def makeBackground(color,app):
     #create some instances of Color(color, alpha)
     #for more info read https://ggame.readthedocs.io/en/latest/ggameapi.html#color
     
@@ -23,23 +23,28 @@ def makeBackground(color):
     #for more info read https://ggame.readthedocs.io/en/latest/ggameapi.html#rectangleasset
     
     # Now display a rectangle
-    rectangle = RectangleAsset(myapp.width, myapp.height, thinline, color)
-    Sprite(rectangle)
+    rectangle = RectangleAsset(app.width, app.height, thinline, color)
+    return Sprite(rectangle)
 
-def addStars(num):
+def addStars(num,app):
     #create an imageAsset 
     #ImageAsset(url, frame=None, qty=1, direction='horizontal', margin=0)
     #https://ggame.readthedocs.io/en/latest/ggameapi.html#imageasset
     starImage = ImageAsset("star.png")
+    
+    #the list of stars to return at the end
+    starList = []
     for i in range(num):
         #create a random position using randrange
         #https://docs.python.org/3/library/random.html#functions-for-integers
-        randomPosition = (random.randrange(myapp.width),random.randrange(myapp.height))
+        randomPosition = (random.randrange(app.width),random.randrange(app.height))
         
         #create a sprite from the image asset
         #https://ggame.readthedocs.io/en/latest/ggameapi.html#sprite
         #Sprite(asset, pos=(0, 0), edgedef=None)
         starSprite = Sprite(starImage,randomPosition)
+        starList.append(starSprite)
+    return starList
         
 if __name__ == "__main__":
     # execute only if run as a script
@@ -47,8 +52,8 @@ if __name__ == "__main__":
     #load the game
     myapp = App()
 
-    makeBackground(black)
-    addStars(50)
+    makeBackground(black,myapp)
+    addStars(50,myapp)
     
     #run the game
     myapp.run()
