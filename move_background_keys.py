c9ba7f317d83fd26e8bd04cb0b8f8b87d16faffb
tiffinschool <62676209+tiffinschool@users.noreply.github.com>
2020-03-26 09:43:15 +0000

    Updated from Brython Server: 26/03/2020 09:43:15

--- move_background_keys.py
+++ move_background_keys.py
@@ -10,7 +10,10 @@ from make_a_space_backdrop import makeBackground, addStars
 
 black = Color(0x000000, 1.0)
 
-
+def moveSpriteDownOnKey(sprite,app,keyname):
+    #bind a key to the keyboard event using listenKeyEvent(eventtype, key, callback)
+    #https://ggame.readthedocs.io/en/latest/ggameapi.html#ggame.app.App.listenKeyEvent
+    app.listenKeyEvent("keydown", keyname, sprite.x += 1)
 
 
 if __name__ == "__main__":
@@ -21,7 +24,10 @@ if __name__ == "__main__":
     
     #these functions are imported in line 8 
     makeBackground(black,myapp)
-    addStars(50,myapp)
+    starList = addStars(50,myapp)
+    #keynames can be found here https://ggame.readthedocs.io/en/latest/ggameapi.html#ggame.event.KeyEvent.key
+    for star of starList:
+        moveSpriteDownOnKey(star,myapp,"up arrow")
     
     #run the game
     myapp.run()
